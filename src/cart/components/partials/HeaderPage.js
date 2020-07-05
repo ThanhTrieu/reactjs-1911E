45bc8b8faf55aefc11e65ad6323b4d01aec918bc
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-07-05 18:53:08 +0700

    update code

--- src/cart/components/partials/HeaderPage.js
+++ src/cart/components/partials/HeaderPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const { Header } = Layout;
@@ -12,7 +13,10 @@ const HeaderPage = () => {
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="3">
+            <NavLink to="/cart">Cart</NavLink>
+          </Menu.Item>
+          
         </Menu>
       </Header>
     </>
